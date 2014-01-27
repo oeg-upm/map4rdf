99b94990f90c1a00bac79fee265ba3d78ae7f5b9
Fransiro <Fransiro@gmail.com>
2014-01-27 11:11:31 +0100

    Add DateUtil for transform Date and AemetIntervalo without warnings problems. Some methods of java.util.Date are deprecated and GWT doesnt implement java.util.Calendar

---
+++ src/main/java/es/upm/fi/dia/oeg/map4rdf/client/util/DateUtil.java
@@ -0,0 +1,23 @@
+package es.upm.fi.dia.oeg.map4rdf.client.util;
+
+import java.util.Date;
+
+import com.google.gwt.i18n.client.DateTimeFormat;
+
+import es.upm.fi.dia.oeg.map4rdf.share.AemetIntervalo;
+
+public class DateUtil {
+	public static Date asDate(AemetIntervalo intervalo) {
+		DateTimeFormat dtf = DateTimeFormat.getFormat(AemetIntervalo.getDateTimeFormat());
+		Date date= new Date();
+		dtf.parse(intervalo.getTimeFormatted(),0, date);
+		//date is modified by dtf
+	    return date;
+	}
+	public static AemetIntervalo asAemetIntervalo(Date date){
+		String timeFormat=AemetIntervalo.getDateTimeFormat();
+		String toConvert=DateTimeFormat.getFormat(timeFormat).format(date);
+		AemetIntervalo toReturn = new AemetIntervalo(toConvert);
+		return toReturn;
+	}
+}
