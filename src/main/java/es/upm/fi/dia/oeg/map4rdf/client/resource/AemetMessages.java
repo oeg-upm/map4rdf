92aad092fba239e22873ff1ec443f924f5d5f8c5
Fransiro <Fransiro@gmail.com>
2014-01-27 11:20:25 +0100

    Add AemetMessages for client browser

---
+++ src/main/java/es/upm/fi/dia/oeg/map4rdf/client/resource/AemetMessages.java
@@ -0,0 +1,28 @@
+package es.upm.fi.dia.oeg.map4rdf.client.resource;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface AemetMessages extends Messages{
+	String station();
+	String noObserveData();
+	String day();
+	String week();
+	String back();
+	String globalRadiation();
+	String pressure();
+	String pressureReducedToSeaLevel();
+	String tempTheDewPoint();
+	String precipitation();
+	String rh();
+	String airTemperature();
+	String maxWindVelocity();
+	String maxWind();
+	String windTour();
+	String avarageWind();
+	String avarageVelocityOfWind();
+	String degrees();
+	String liters();
+	String loadingChart();
+	String charts();
+	String tittleChartOf(String tittle,String time,String site);
+}
