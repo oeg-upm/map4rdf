583949ca1f12ec8020615da73fdca5966366fd56
Fransiro <Fransiro@gmail.com>
2014-02-19 12:56:52 +0100

    Added messages for WebNMas1 Model.

---
+++ src/main/java/es/upm/fi/dia/oeg/map4rdf/client/resource/WebNMasUnoMessages.java
@@ -0,0 +1,10 @@
+package es.upm.fi.dia.oeg.map4rdf.client.resource;
+
+import com.google.gwt.i18n.client.Messages;
+
+public interface WebNMasUnoMessages extends Messages{
+	String guides();
+	String trips();
+	String noGuidesAndTrips();
+	String notGuidesForDateFilter();
+}
