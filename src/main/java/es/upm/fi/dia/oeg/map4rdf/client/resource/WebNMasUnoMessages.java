02b5adcb0b85699583caa2cd5ff2ebe130f17788
Fransiro <Fransiro@gmail.com>
2014-02-21 12:11:36 +0100

    Added other info of trips  menssages.

--- src/main/java/es/upm/fi/dia/oeg/map4rdf/client/resource/WebNMasUnoMessages.java
+++ src/main/java/es/upm/fi/dia/oeg/map4rdf/client/resource/WebNMasUnoMessages.java
@@ -3,9 +3,17 @@ package es.upm.fi.dia.oeg.map4rdf.client.resource;
 import com.google.gwt.i18n.client.Messages;
 
 public interface WebNMasUnoMessages extends Messages{
-	String guides();
-	String trips();
-	String noGuidesAndTrips();
-	String notGuidesForDateFilter();
-	String notTripsForDateFilter();
+	public String guides();
+	public String trips();
+	public String noGuidesAndTrips();
+	public String notGuidesForDateFilter();
+	public String notTripsForDateFilter();
+	public String title();
+	public String priceLess();
+	public String priceMore();
+	public String durationLess();
+	public String durationMore();
+	public String distanceLess();
+	public String distanceMore();
+	public String description();
 }
