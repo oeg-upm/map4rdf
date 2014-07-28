42319ba498faa80dfd127fe370a106cd9b3f08a9
Fransiro <Fransiro@gmail.com>
2014-07-28 12:34:08 +0200

    Change logos folder

--- src/main/java/es/upm/fi/dia/oeg/map4rdf/share/conf/ConstantsFolders.java
+++ src/main/java/es/upm/fi/dia/oeg/map4rdf/share/conf/ConstantsFolders.java
@@ -2,6 +2,6 @@ package es.upm.fi.dia.oeg.map4rdf.share.conf;
 
 public class ConstantsFolders {
 
-	public final static String LOGOS_FOLDER="logos";
+	public final static String LOGOS_FOLDER="logos/";
 	
 }
